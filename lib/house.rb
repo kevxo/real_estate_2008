6c91deeece406732a7a53fb87004f48cd88d6e14
Kevin Cuadros <kev040798@gmail.com>
2020-08-26 12:00:27 -0400

    Iteration three

--- lib/house.rb
+++ lib/house.rb
@@ -11,4 +11,34 @@ class House
   def add_room(room)
     @rooms << room
   end
+
+  def above_market_average?
+    return false if @price.to_i < 500000
+    true
+  end
+
+  def rooms_from_category(category)
+    room_array = []
+    @rooms.each do |room|
+      if category == room.category
+        room_array << room
+      end
+    end
+    room_array
+  end
+
+  def area
+    total = []
+    @rooms.each do |room|
+      total << room.length * room.width.to_i
+    end
+    total.sum
+  end
+
+  def details
+    house_details = {}
+    house_details["price"] = @price
+    house_details["address"] =  @address
+    house_details
+  end
 end
--- test/house_test.rb
+++ test/house_test.rb
@@ -29,4 +29,50 @@ class HouseTest < Minitest::Test
 
     assert_equal [room_1, room_2], house.rooms
   end
+
+  def test_it_is_above_market_average
+    house = House.new("$400000", "123 sugar lane")
+
+    assert_equal false, house.above_market_average?
+  end
+
+
+  def test_it_returns_array_of_category_rooms
+    house = House.new("$400000", "123 sugar lane")
+    room_1 = Room.new(:bedroom, 10, '13')
+    room_2 = Room.new(:bedroom, 11, '15')
+    room_3 = Room.new(:living_room, 25, '15')
+    room_4 = Room.new(:basement, 30, '41')
+
+    house.add_room(room_1)
+    house.add_room(room_2)
+    house.add_room(room_3)
+    house.add_room(room_4)
+
+    assert_equal [room_1, room_2], house.rooms_from_category(:bedroom)
+    assert_equal [room_4], house.rooms_from_category(:basement)
+  end
+
+  def test_it_returns_area_1900
+    house = House.new("$400000", "123 sugar lane")
+    room_1 = Room.new(:bedroom, 10, '13')
+    room_2 = Room.new(:bedroom, 11, '15')
+    room_3 = Room.new(:living_room, 25, '15')
+    room_4 = Room.new(:basement, 30, '41')
+
+    house.add_room(room_1)
+    house.add_room(room_2)
+    house.add_room(room_3)
+    house.add_room(room_4)
+
+    assert_equal 1900, house.area
+  end
+
+
+  def test_it_returns_house_details
+    house = House.new("$400000", "123 sugar lane")
+
+    expected = {"price" => "$400000", "address" => "123 sugar lane"}
+    assert_equal expected, house.details
+  end
 end
